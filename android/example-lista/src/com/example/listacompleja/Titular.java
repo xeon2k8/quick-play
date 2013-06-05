eacb1408a791fa8459f4e3c8f72f5f3dcdad53e6
luchoruiz <lucianof.ruiz@gmail.com>
2013-06-05 17:05:23 -0300

    New List Example

---
+++ android/example-lista/src/com/example/listacompleja/Titular.java
@@ -0,0 +1,21 @@
+package com.example.listacompleja;
+
+public class Titular {
+	
+	    private String titulo;
+	    private String subtitulo;
+	 
+	    public Titular(String tit, String sub){
+	        titulo = tit;
+	        subtitulo = sub;
+	    }
+	 
+	    public String getTitulo(){
+	        return titulo;
+	    }
+	 
+	    public String getSubtitulo(){
+	        return subtitulo;
+	    }
+	}
+
